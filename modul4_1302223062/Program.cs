81d1a14aaf66c8991b603898ee529e4d483eed23
isabellamapoe <bellamanope@gmail.com>
2024-03-16 08:45:32 +0700

    menambahkan class KodeBuah

--- modul4_1302223062/Program.cs
+++ modul4_1302223062/Program.cs
@@ -5,26 +5,44 @@ public class KodeBuah
 {
     public enum Buah
     {
-        Apel, 
-        Aprikot, 
+        Apel,
+        Aprikot,
         Alpukat,
-        Pisang, 
-        Paprika, 
-        Blackberry, 
-        Ceri, 
-        Kelapa, 
-        Jagung, 
-        Kurma, 
-        Durian, 
-        Angggur, 
-        Melon, 
+        Pisang,
+        Paprika,
+        Blackberry,
+        Ceri,
+        Kelapa,
+        Jagung,
+        Kurma,
+        Durian,
+        Angggur,
+        Melon,
         Semangka
     }
 
     public static string getKodeBuah(Buah buah)
     {
-        string[] kodeBuah = {"A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
-        return kodeBuah[(int)buah] ;
+        string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+        return kodeBuah[(int)buah];
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kode = getKodeBuah(Buah.Paprika);
+            Console.WriteLine("paprika, " + kode);
+
+            kode = getKodeBuah(Buah.Durian);
+            Console.WriteLine("Durian, " + kode);
+
+            kode = getKodeBuah(Buah.Semangka);
+            Console.WriteLine("semangka, " + kode);
+
+            kode = getKodeBuah(Buah.Apel);
+            Console.WriteLine("Apel, " + kode);
+        }
     }
 
 }
